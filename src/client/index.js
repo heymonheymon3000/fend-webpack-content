efa196ce578c278aab8389cfa2ac8229a77c05c1
Terry Parrish <tparri2@emory.edu>
2020-05-06 18:32:57 -0400

    update

--- src/client/index.js
+++ src/client/index.js
@@ -1,1 +1,10 @@
+import { checkForName } from './js/nameChecker'
+import { handleSubmit } from './js/formHandler'
+const json = require('./file.json');
+
+console.log(checkForName());
+console.log(json.firstName);
+
+
+alert("I EXIST")
 alert("I am here!!!");
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,27 @@
 const path = require('path')
 const webpack = require('webpack')
+const HtmlWebPackPlugin = require('html-webpack-plugin')
 
 module.exports = {
-    entry: "./src/client/index.js"
+    mode: "development",
+    entry: "./src/client/index.js",
+    module: {
+        rules: [
+            {
+                test: '/\.js$/',
+                exclude: /node_modules/,
+                loader: "babel-loader"
+            },
+            {
+                test: '/\.json$/',
+                loader: "json-loader"
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebPackPlugin({
+            template: "./src/client/views/index.html",
+            filename: "./index.html",
+        })
+    ]
 }
